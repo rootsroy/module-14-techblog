fa822658539064c19380f700d4cc8fb658add775
Ruth Torres <ruths.roy@gmail.com>
2021-10-19 04:36:56 -0700

    added logout and login feature

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require("express");
-
+const sequelize = require("./config/connection");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//  creates session
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+require("dotenv").config();
+
+const sess = {
+  secret: process.env.DB_SECRET,
+  cookie: {},
+  resave: false,
+  saveUnitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// uses public folder
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 
 // sets handlebars as template engine
 const exphbs = require("express-handlebars");
@@ -12,16 +33,12 @@ const hbs = exphbs.create({});
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-// turn on public folder
-const path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
-
 // turn on routes
 const routes = require("./controllers");
 app.use(routes);
 
 // turn on connection to db and server
-const sequelize = require("./config/connection");
+
 const PORT = process.env.PORT || 3001;
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
